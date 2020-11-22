2746a4febd1ed6901e7d5958b69ef51fe290dbe2
altheaabadilla2599 <70379536+altheaabadilla2599@users.noreply.github.com>
2020-11-22 21:50:51 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,967 @@
+
+<title> Floom </title>
+
+<style>
+
+
+
+body{
+	background-color: #88aed0;
+	min-height: 100vh;	
+	padding-bottom: 300px;
+	position: relative;
+}
+
+
+
+#logo {
+	font-family: Verdana;
+	color: #000;
+	font-size: 65px;
+	margin-left: 480px;
+	margin-top: 40px;
+}
+#logo1 {
+	font-family: Verdana;
+	color: #000;
+	font-size: 65px;
+	margin-left: 180px;
+	margin-top: -90px;
+}
+#logo2 {
+	font-family: Verdana;
+	color: #000;
+	font-size: 65px;
+	margin-left: 240px;
+	margin-top: -65px;
+}
+
+#holder{
+	width: 410px;
+	height: 190px;
+	background-color: transparent;
+	margin-top: -150px;
+	margin-left: 20px;
+
+}
+
+#holder2{
+	width: 410px;
+	height: 190px;
+	background-color: transparent;
+	margin-left: 850px;
+	margin-top: -185px;
+	
+
+}
+#holder3{
+	width: 100px;
+	height: 100px;
+	background-color: transparent;
+	margin-left: 780px;
+	margin-top: -160px;
+	animation: rotate 5s linear infinite;
+}
+
+
+  
+#holder4{
+	width: 90px;
+	height: 80px;
+	background-color: transparent;
+	margin-left: 10px;
+	margin-top: -100px;
+	
+}
+#holder5{
+	width: 50px;
+	height: 50px;
+	background-color: tranparent;
+	margin-left: 10px;
+	
+}
+#holder6{
+	width: 40px;
+	height: 40px;
+	background-color: tranparent;
+	margin-left: -10px;
+	margin-top: -80px;
+	
+}
+#holder7{
+	width: 100px;
+	height: 110px;
+	background-color: tranparent;
+	margin-left: 1240px;
+	margin-top: -80px;
+
+	
+}
+#holder8{
+	width: 50px;
+	height: 50px;
+	background-color: tranparent;
+	margin-left: 510px;
+	margin-top: -120px;
+
+	
+}
+#holder9{
+	width: 40px;
+	height: 40px;
+	background-color: tranparent;
+	margin-left: 570px;
+	margin-top: 40px;
+
+	
+}
+
+#holder10{
+	width: 50px;
+	height: 50px;
+	background-color:tranparent;
+	margin-left: 695px;
+	margin-top: -120px;
+	
+}
+
+#holder11{
+	width: 50px;
+	height: 50px;
+	background-color: transparent;
+	margin-left: 650px;
+	margin-top: -130px;
+}
+
+#holder12{
+	width: 300px;
+	height: 400px;
+	background-color: tranparent;
+	margin-left: 200px;
+	margin-top: 100px;
+	
+}
+
+
+
+
+#holder13{
+	width: 40px;
+	height: 40px;
+	background-color: tranparent;
+	margin-left: 780px;
+	margin-top: 50px;
+}
+
+
+#title {
+
+	width: 700px;
+	height: 250px;
+	background-color: tranparent;
+	margin-left: 320px;
+	margin-top: 20px;
+}	
+
+
+#title3 {
+
+	font-family: Verdana;
+	font-size:50px;
+	margin-left: 370px;
+	color: white;
+	margin-top: 70px;
+}	
+
+
+#holder14{
+	width: 150px;
+	height: 130px;
+	background-color: tranparent;
+	margin-left: 50px;
+	margin-top: -400px;
+	animation: tilt 10s;
+	animation-iteration-count: infinite;
+	
+}
+
+@keyframes tilt {
+0% {
+   transform: rotate(30deg);
+}
+20% {
+ transform: rotate(0deg);
+ }
+ 40% {
+ transform: rotate(-30deg);
+ } 
+ 60% {
+  transform: rotate(0deg);
+}
+80% {
+  transform: rotate(30deg);
+}
+  100% {
+  transform: rotate(0deg);
+}
+}
+#holder15{
+	width: 150px;
+	height: 120px;
+	margin-left: 500px;
+	background-color: tranparent;
+	margin-top: -125px;
+	animation: tilt 10s;
+	animation-iteration-count: infinite;
+	
+}
+
+
+
+@keyframes tilt {
+0% {
+   transform: rotate(30deg);
+}
+20% {
+ transform: rotate(0deg);
+ }
+ 40% {
+ transform: rotate(-30deg);
+ } 
+ 60% {
+  transform: rotate(0deg);
+}
+80% {
+  transform: rotate(30deg);
+}
+  100% {
+  transform: rotate(0deg);
+}
+}
+
+
+#holder16{
+	width: 100px;
+	height: 90px;
+	background-color: transparent;
+	margin-left: 1050px;
+	margin-top: -345px;	
+	animation: rotate 5s linear infinite;
+
+}
+
+#holder17 {
+	width: 100px;
+	height: 90px;
+	background-color: transparent;
+	margin-left: 520px;
+	margin-top: -140px;	;
+
+}
+
+
+#about {
+	font-family: 'Montserrat', sans-serif;
+	font-size:50px;
+	margin-left: 700px;
+	color: white;
+	margin-top: -150px;	
+	position: absolute;
+	letter-spacing:0.1em;
+	-webkit-text-fill-color: transparent;
+	-webkit-text-stroke-width: 3px;
+	-webkit-text-stroke-color: white;
+	text-shadow: 
+						5px 5px #ff1f8f,
+						8px 8px red;
+}
+
+
+
+
+#about1 {
+	font-family: Rockwell;
+	font-size:25px;
+	margin-left: 710px;
+	margin-top: -80px;	
+	position: absolute;
+	letter-spacing:0.1em;
+	-webkit-text-fill-color: transparent;
+	-webkit-text-stroke-width: 1px;
+	-webkit-text-stroke-color: white;
+}
+
+#about2 {
+	font-family: Verdana;
+	font-size: 18px;
+	margin-left: 710px;
+	margin-top: -10px;	
+	
+}
+
+#about3 {
+	font-family: Verdana;
+	font-size:18px;
+	margin-left: 710px;
+	margin-top: 60px;	
+	
+}
+
+
+
+a
+{
+	position: absolute;
+	margin-top: -450px;
+	margin-left: 700px;
+	width: 180px;
+	height: 60px;
+	text-align:center;
+	font-family: Rockwell;
+	text-transform: uppercase;
+	font-size: 30px;
+	letter-spacing: 2px;
+	color: fff;
+	text-decoration: none;
+}
+
+#container {
+	width: 300px;
+	height: 400px;
+	background-color: tranparent;
+	margin-left: -10px;
+	margin-top: -100px;
+}
+
+#whytext {
+	font-family: Rockwell;
+	font-size:40px;
+	margin-left: 310px;
+	margin-top: 150px;	
+	position: absolute;
+	letter-spacing:0.1em;
+	-webkit-text-fill-color: transparent;
+	-webkit-text-stroke-width: 2px;
+	-webkit-text-stroke-color: black;
+}
+
+svg {
+  margin-left: 125px;
+   margin-top: -250px;
+}
+
+#lid {
+  animation: blink 2.4s infinite;
+}
+#eye {
+  animation: squeeze 2.4s infinite;
+}
+
+@keyframes blink {
+  90% {
+    transform: none;
+    animation-timing-function: ease-in;
+  }
+  93% {
+    transform: translateY(15px) scaleY(0)
+  }
+  100% {
+    animation-timing-function: ease-out;
+  }
+}
+@keyframes squeeze {
+  90% {
+    transform: none;
+    animation-timing-function: ease-in;
+  }
+  93% {
+    transform: translateY(3px) scaleY(0.8)
+  }
+  100% {
+    animation-timing-function: ease-out;
+  }
+}
+
+#whytext2 {
+	font-family: 'Montserrat', sans-serif;
+	font-size:20px;
+	margin-left: 35px;
+	color: white;
+	margin-top: -10px;	
+	position: absolute;
+	letter-spacing:0.1em;
+	-webkit-text-fill-color: transparent;
+	-webkit-text-stroke-width: 1px;
+	-webkit-text-stroke-color: black;
+	
+}
+
+#whytext3 {
+	font-family: Calibri;
+	font-size:20px;
+	color:black;
+	margin-left: 35px;
+	margin-top: 40px;	
+	
+}
+#whytext4 {
+	font-family: 'Montserrat', sans-serif;
+	font-size:20px;
+	margin-left: 450px;
+	color: white;
+	margin-top: -225px;	
+	position: absolute;
+	letter-spacing:0.1em;
+	-webkit-text-fill-color: transparent;
+	-webkit-text-stroke-width: 1px;
+	-webkit-text-stroke-color: black;
+	
+}
+
+#whytext5 {
+	font-family: Calibri;
+	font-size:20px;
+	color:black;
+	margin-left: 450px;
+	margin-top: -170px;	
+	
+}
+
+#whytext6 {
+	font-family: 'Montserrat', sans-serif;
+	font-size:20px;
+	margin-left: 900px;
+	color: white;
+	margin-top: -188px;	
+	position: absolute;
+	letter-spacing:0.1em;
+	-webkit-text-fill-color: transparent;
+	-webkit-text-stroke-width: 1px;
+	-webkit-text-stroke-color: black;
+	
+}
+#whytext7 {
+	font-family: Calibri;
+	font-size:20px;
+	color:black;
+	margin-left: 910px;
+	margin-top: -130px;	
+	
+}
+#txt {
+	font-family: Verdana;
+	font-size:20px;
+	color:black;
+	margin-top: -290px;	
+	margin-left: 300px;
+
+}
+
+#txt a{
+	font-family: Verdana;
+	font-size:20px;
+	color:black;
+	margin-top: -10px;	
+	margin-left: -100px;
+
+}
+
+#txt2 {
+	font-family: Verdana;
+	font-size:20px;
+	color:black;
+	margin-top: -290px;	
+	margin-left: 450px;
+
+}
+
+#txt2 a{
+	font-family: Verdana;
+	font-size:20px;
+	color:black;
+	margin-top: -10px;	
+	margin-left: -100px;
+
+}
+
+#txt3 {
+	font-family: Verdana;
+	font-size:20px;
+	color:black;
+	margin-top: -290px;	
+	margin-left: 630px;
+
+}
+
+#txt3 a{
+	font-family: Verdana;
+	font-size:20px;
+	color:black;
+	margin-top: -10px;	
+	margin-left: -100px;
+
+}
+
+#txt4 {
+	font-family: Verdana;
+	font-size:20px;
+	color:black;
+	margin-top: -290px;	
+	margin-left: 800px;
+
+}
+
+#txt4 a{
+	font-family: Verdana;
+	font-size:20px;
+	color:black;
+	margin-top: -10px;	
+	margin-left: -100px;
+
+}
+
+#txt5 {
+	font-family: Verdana;
+	font-size:20px;
+	color:black;
+	margin-top: -290px;	
+	margin-left: 980px;
+
+}
+
+#txt5 a{
+	font-family: Verdana;
+	font-size:20px;
+	color:black;
+	margin-top: -10px;	
+	margin-left: -100px;
+
+}
+
+#txt6 {
+	font-family: Verdana;
+	font-size:20px;
+	color:black;
+	margin-top: 100px;	
+	margin-left: 570px;
+
+}
+
+#txt7 {
+	font-family: Verdana;
+	font-size:20px;
+	color:black;
+	margin-top: -30px;	
+	margin-left: 630px;
+
+}
+
+#txt8 {
+	font-family: Verdana;
+	font-size:20px;
+	color:black;
+	margin-top: -18px;	
+	margin-left: 650px;
+}
+#txt9 {
+	font-family: Verdana;
+	font-size:12px;
+	color:black;
+	margin-top: 10px;	
+	margin-left: 490px;
+
+}
+
+
+
+
+
+
+
+
+#holder18{
+	width: 150px;
+	height: 90px;
+	margin-left: 1000px;
+	background-color: tranparent;
+	margin-top: -150px;
+	position: absolute;
+	animation: slide 12s linear 0s infinite;
+}
+
+@keyframes slide {
+  from {
+    right: 0;
+  } 
+  to {
+    right: 1700px;
+  } 
+}
+
+#holder19 {
+	width: 100px;
+	height: 90px;
+	background-color: transparent;
+	margin-left: 100px;
+	margin-top:	-170px;	
+	position:absolute;
+	animation: line 12s linear 0s infinite;
+}
+@keyframes line {
+  from {
+    left: 0;
+  } 
+  to {
+    left: 1300px;
+  } 
+}
+
+footer{
+	width: 100%;
+	position: absolute;
+	bottom: 0;
+	margin-left:-10px;
+	margin-top: 200px;
+}
+#holder20 {
+	width: 100px;
+	height: 90px;
+	background-color: black;
+	margin-left: 100px;
+	margin-top:	-200px;	
+	position:absolute;
+}
+
+
+#holder21{
+	width: 100px;
+	height: 90px;
+	margin-left: 340px;
+	background-color: transparent;
+	margin-top: -100px;
+	position: absolute;
+}
+
+#holder21 a{
+	width: 50px;
+	height: 50px;
+	margin-top: -150px;
+	
+}
+
+#holder22{
+	width: 100px;
+	height: 90px;
+	margin-left: 420px;
+	background-color: transparent;
+	margin-top: -95px;
+	position: absolute;
+}
+
+#holder22 a{
+	width: 50px;
+	height: 50px;
+	margin-top: -155px;
+	
+}
+
+#holder23{
+	width: 100px;
+	height: 90px;
+	margin-left: 500px;
+	background-color: transparent;
+	margin-top: -95px;
+	position: absolute;
+}
+
+#holder23 a{
+	width: 50px;
+	height: 50px;
+	margin-top: -155px;
+	
+
+
+
+
+</style>
+
+
+
+
+<html>
+<head>
+
+</head>
+
+<body>
+
+
+	<div id="logo"> F L O
+	<div id="logo1"> O</div>
+	<div id="logo2"> M</div>
+	</div>
+	
+<div id="holder">
+
+<img src="design2.svg">
+
+</div>
+
+<div id="holder2">
+<img src="design22.svg">
+</div>
+
+
+<div id="holder3">
+
+<img src="sun.svg">
+
+</div>
+<div id="holder4">
+
+<img src="butterfly.svg">
+</div>
+
+<div id="holder5">
+
+
+<img src="butterfly2.svg">
+</div>
+
+<div id="holder6">
+
+
+<img src="butterfly4.svg">
+</div>
+
+<div id="holder7">
+<img src="butterfly3.svg">
+</div>
+
+<div id="holder8">
+<img src="butterfly5.svg">
+</div>
+
+<div id="holder9">
+<img src="butterfly7.svg">
+</div>
+
+
+
+<div id="holder11">
+<img src="butterfly8.svg">
+</div>
+
+
+
+<div id="holder13">
+<img src="butterfly10.svg">
+</div>
+
+
+
+<div id="title">
+<img src="signage.svg">
+</div>
+
+
+
+<div id="holder12">
+<img src="bouquet.svg">
+</div>
+
+<div id="holder14">
+
+<img src="basket1.svg">
+
+</div>
+
+<div id="holder15">
+
+
+<img src="basket2.svg">
+
+</div>
+
+<div id="about">
+	OUR STORY
+	</div>
+
+<div id="about1">
+	Our inspiration comes from you
+	</div>
+	
+<div id="about2">
+	Three years ago in London, Lana Elie founded Floom,
+	an online marketplace that provides independent florists
+	from the UK and US a platform to sell beautifully crafted
+	bouquets, and customers a way to buy them.
+	</div>
+	
+<div id="about3">
+	Frustrated with the underwhelming arrangements big floral companies offered online, 
+	Lana knew there had to be a better way to find and send arrangements that didn’t feel
+	prepackaged and impersonal. The answer laid in handpicking local independent florists
+	who were experts at creating thoughtful, unique bouquets. (The rest is botanic history!)
+	</div>
+	
+<div id="whytext">
+	Why send flowers with Floom?
+	</div>
+
+
+
+<div id="container">
+	<img src="background.svg">
+	<div id="holder16">
+	<img src="flower3.svg" width="100px" height="100px">
+
+	</div>
+	<div id="holder17">
+	<img src="smile.svg">
+
+	</div>
+	</div>
+	
+<div id="whytext2">
+	What you see is what you get
+	</div>
+	
+<div id="whytext3">
+	<p>Love the bouquet on your screen?</p>
+	<p>That’s exactly what our local florist</p>
+	<p>will prepare freshly for your order…</p>
+	<p>or your money back!</p>
+	</div>
+	
+<div id="whytext4">
+	Always unique, never generic
+	</div>
+	
+<div id="whytext5">
+	<p>We only work with the most talented</p>
+	<p>and unique artisans, and we’re passionate</p>
+	<p>about supporting our skilled family of florists.</p>
+	</div>
+	
+<div id="whytext6">
+	Hand-delivered with care and attention
+	</div>
+
+
+
+<div id="whytext7">
+	<p>Each of our orders is professionally arranged,</p>
+	<p>wrapped and safely delivered with a hand-written</p>
+	<p>card… on the exact day that you need it.</p>
+
+	</div>
+	
+	
+	
+	
+<svg width="100" height="100" viewBox="-10 10 50 30" version="1.1"  xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg">
+    <defs>
+        <path d="M0,15.089434 C0,16.3335929 5.13666091,24.1788679 14.9348958,24.1788679 C24.7325019,24.1788679 29.8697917,16.3335929 29.8697917,15.089434 C29.8697917,13.8456167 24.7325019,6 14.9348958,6 C5.13666091,6 0,13.8456167 0,15.089434 Z" id="outline"></path>
+        <mask id="mask">
+          <rect width="100%" height="100%" fill="white"></rect>
+          <use xlink:href="#outline" id="lid" fill="black"/>
+        </mask>
+    </defs>
+    <g id="eye">
+        <path d="M0,15.089434 C0,16.3335929 5.13666091,24.1788679 14.9348958,24.1788679 C24.7325019,24.1788679 29.8697917,16.3335929 29.8697917,15.089434 C29.8697917,13.8456167 24.7325019,6 14.9348958,6 C5.13666091,6 0,13.8456167 0,15.089434 Z M14.9348958,22.081464 C11.2690863,22.081464 8.29688487,18.9510766 8.29688487,15.089434 C8.29688487,11.2277914 11.2690863,8.09740397 14.9348958,8.09740397 C18.6007053,8.09740397 21.5725924,11.2277914 21.5725924,15.089434 C21.5725924,18.9510766 18.6007053,22.081464 14.9348958,22.081464 L14.9348958,22.081464 Z M18.2535869,15.089434 C18.2535869,17.0200844 16.7673289,18.5857907 14.9348958,18.5857907 C13.1018339,18.5857907 11.6162048,17.0200844 11.6162048,15.089434 C11.6162048,13.1587835 13.1018339,11.593419 14.9348958,11.593419 C15.9253152,11.593419 14.3271242,14.3639878 14.9348958,15.089434 C15.451486,15.7055336 18.2535869,14.2027016 18.2535869,15.089434 L18.2535869,15.089434 Z" fill="#ffd300"></path>
+        <use xlink:href="#outline" mask="url(#mask)" fill="#FFFFFF"/>
+    </g>
+</svg>
+
+
+
+
+ 
+
+ 
+
+<a href="https://www.floom.com/shop/flowers">
+	<svg>
+	<rect></rect>
+	</svg>
+	SHOP NOW
+	</a>
+	
+<footer>
+<img src="background2.svg">
+
+<div id="holder18">
+<img src="design3.svg" width="100px" height="100px">
+</div>
+
+<div id="holder19">
+<img src="design4.svg" width="100px" height="100px">
+</div>
+<div id="holder21">
+<a href="https://www.facebook.com/FloomOfficial"><img src="facebook.svg" width="70px" height="70px"></a>
+</div>
+<div id="holder22">
+<a href="http://www.twitter.com/floomofficial"><img src="twitter.svg" width="70px" height="70px"></a>
+</div>
+<div id="holder23">
+<a href="https://www.instagram.com/floomofficial"><img src="instagram.svg" width="70px" height="70px"></a>
+</div>
+
+<div id="txt">
+	<a href="https://www.floom.com/pages/faq">FAQS</a>
+
+	</div>
+	
+<div id="txt2">
+	<a href="https://www.floom.com/magazine">MAGAZINE</a>
+
+	</div>
+<div id="txt3">
+	<a href="https://www.floom.com/pages/contact-us">CONTACT US</a>
+
+	</div>
+	
+<div id="txt4">
+	<a href="https://www.floom.com/pages/about-us">ABOUT US</a>
+
+	</div>
+	
+	
+<div id="txt5">
+	<a href="https://www.floom.com/login">MY ACCOUNT</a>
+
+	</div>
+<div id="txt6">
+	F L O
+
+	</div>
+	
+<div id="txt7">
+	O
+
+	</div>
+
+<div id="txt8">
+	M
+
+	</div>
+	
+<div id="txt9">
+	© 2020 / FLOOM.LTD / ALL RIGHTS RESERVED
+	
+	</div>
+	
+
+
+
+</footer>
+	
+
+ </body>
+ 
+
+
+
+
+
